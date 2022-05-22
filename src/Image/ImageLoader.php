645083e984fe6b3f7237b710b5c3d2b72ea11641
Ivan Puzatkin <puzatkin1998@gmail.com>
2022-05-22 23:52:21 +0300

    refactor image loader

--- src/Image/ImageLoader.php
+++ src/Image/ImageLoader.php
@@ -10,10 +10,13 @@ use GuzzleHttp\Promise\Utils;
 
 class ImageLoader
 {
+    /**
+     * @param string $url
+     * @param array $images
+     * @return array
+     */
     public function load(string $url, array $images): array
     {
-        $images_to_download = $images;
-
         $guzzle_client = new Client([
             'timeout' => 180.0, // see how i set a timeout
             'handler' => HandlerStack::create(new CurlMultiHandler([
@@ -23,47 +26,38 @@ class ImageLoader
                 ]
             ]))
         ]);
-        $attempts = [];
-        $images_ready = [];
-        for ($i = 1; $i <= 3; $i++) {
 
-            $requestPromises = [];
-            $temp_files = [];
-            foreach ($images_to_download as $image_name) {
-                $imageUrl = sprintf('%s/%s', $url, $image_name);
-                $tmpFile = $this->generateTempFileName($image_name);
-                $temp_files[$image_name] = $tmpFile;
-                $requestPromises[$image_name] = $guzzle_client->getAsync($imageUrl, [
-                    'sink' => $tmpFile,
-                    'connect_timeout' => 3.14
-                ]);
-                $attempts[$image_name] = empty($attempts[$image_name]) ? 1 : $attempts[$image_name] + 1;
-            }
+        $tempFiles = [];
+        $requestPromises = [];
+        foreach ($images as $imageName) {
+            $imageUrl = sprintf('%s/%s', $url, $imageName);
+            $tmpFile = $this->generateTempFileName($imageName);
+            $tempFiles[$imageName] = $tmpFile;
+            $requestPromises[$imageName] = $guzzle_client->getAsync($imageUrl, [
+                'sink' => $tmpFile,
+                'connect_timeout' => 3.14
+            ]);
+        }
 
-            $images_to_download = [];
-            $results = Utils::settle($requestPromises)->wait();
+        $imagesReady = [];
+        $lackingImages = [];
+        foreach (array_chunk($requestPromises, 20) as $promises) {
+            $results = Utils::settle($promises)->wait();
             foreach ($results as $image_name => $image_result) {
                 if (
                     $image_result['state'] !== PromiseInterface::FULFILLED
-                    || !file_exists($temp_files[$image_name])
-                    || (filesize($temp_files[$image_name]) === 0)
+                    || !file_exists($tempFiles[$image_name])
+                    || (filesize($tempFiles[$image_name]) === 0)
                 ) {
-                    $images_to_download[] = $image_name;
-                    unlink($temp_files[$image_name]);
+                    $lackingImages[] = $image_name;
+                    unlink($tempFiles[$image_name]);
                 } else {
-                    $images_ready[$image_name] = $temp_files[$image_name];
+                    $imagesReady[$image_name] = $tempFiles[$image_name];
                 }
             }
         }
 
-        $lacking_images = [];
-        foreach ($images as $image_name) {
-            if (empty($images_ready[$image_name])) {
-                $lacking_images[] = $image_name;
-            }
-        }
-
-        return [$images_ready, $lacking_images];
+        return [$imagesReady, $lackingImages];
     }
 
     /**
