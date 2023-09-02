29af4d6c2fc08f179bba3feccc4364afe4c35722
Georg Andrews <daemaken@gmail.com>
2023-09-02 05:49:17 +0300

    improve image download

--- src/Image/ImageLoader.php
+++ src/Image/ImageLoader.php
@@ -2,58 +2,40 @@
 
 namespace Verstka\Image;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Handler\CurlMultiHandler;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Promise\PromiseInterface;
-use GuzzleHttp\Promise\Utils;
+use GuzzleHttp\Client as Guzzle;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Response;
 
 class ImageLoader
 {
     /**
      * @param string $url
      * @param array $images
+     *
      * @return array
      */
     public function load(string $url, array $images): array
     {
-        $guzzle_client = new Client([
-            'timeout' => 180.0, // see how i set a timeout
-            'handler' => HandlerStack::create(new CurlMultiHandler([
-                'options' => [
-                    CURLMOPT_MAX_TOTAL_CONNECTIONS => 20,
-                    CURLMOPT_MAX_HOST_CONNECTIONS => 20,
-                ]
-            ]))
-        ]);
-
-        $tempFiles = [];
-        $requestPromises = [];
+        $imagesForDownload = [];
         foreach ($images as $imageName) {
-            $imageUrl = sprintf('%s/%s', $url, $imageName);
-            $tmpFile = $this->generateTempFileName($imageName);
-            $tempFiles[$imageName] = $tmpFile;
-            $requestPromises[$imageName] = $guzzle_client->getAsync($imageUrl, [
-                'sink' => $tmpFile,
-                'connect_timeout' => 3.14
-            ]);
+            $imagesForDownload[$imageName] = [
+                'download_from' => sprintf('%s/%s', $url, $imageName),
+                'download_to' => $this->generateTempFileName($imageName)
+            ];
         }
+
+        $imagesForDownload = $this->downloadImages($imagesForDownload); // Attempt number one
+        $imagesForDownload = $this->downloadImages($imagesForDownload); // Attempt number two
+        $imagesForDownload = $this->downloadImages($imagesForDownload); // Attempt number three
 
         $imagesReady = [];
         $lackingImages = [];
-        foreach (array_chunk($requestPromises, 20, true) as $promises) {
-            $results = Utils::settle($promises)->wait();
-            foreach ($results as $image_name => $image_result) {
-                if (
-                    $image_result['state'] !== PromiseInterface::FULFILLED
-                    || !file_exists($tempFiles[$image_name])
-                    || (filesize($tempFiles[$image_name]) === 0)
-                ) {
-                    $lackingImages[] = $image_name;
-                    unlink($tempFiles[$image_name]);
-                } else {
-                    $imagesReady[$image_name] = $tempFiles[$image_name];
-                }
+        foreach ($imagesForDownload as $imageName => $imageData) {
+            if ($imageData['is_lacking'] === true) {
+                $lackingImages[] = $imageName;
+            } else {
+                $imagesReady[$imageName] = $imageData['download_to'];
             }
         }
 
@@ -61,9 +43,70 @@ class ImageLoader
     }
 
     /**
+     * @param string $imageName
+     *
+     * @return string
+     */
+    private function generateTempFileName(string $imageName): string
+    {
+        return tempnam(sys_get_temp_dir(), str_replace('.', '_', uniqid('vms_' . microtime(true) . '_' . $imageName)));
+    }
+
+    /**
+     * @param $imagesForDownload
+     *
+     * @return array
+     */
+    private function downloadImages($imagesForDownload): array
+    {
+        $client = new Guzzle(['connect_timeout' => 3.14, 'timeout' => 180.0]);
+        $requestPromises = function ($imagesForDownload) use ($client) {
+            foreach ($imagesForDownload as $imageName => $imageData) {
+                if (isset($imageData['is_lacking']) && $imageData['is_lacking'] === false) {
+                    continue; // Skip successful ones on retry
+                }
+                yield $imageName => function () use ($client, $imageName, $imageData) {
+                    return $client->getAsync($imageData['download_from'], [
+                        'sink' => $imageData['download_to']
+                    ]);
+                };
+            }
+        };
+
+        $pool = new Pool($client, $requestPromises($imagesForDownload), [
+            'concurrency' => 20,
+            'fulfilled' => function (Response $response, $imageName) use (&$imagesForDownload) {
+                $imagesForDownload[$imageName]['is_lacking'] = false;
+            },
+            'rejected' => function (RequestException $reason, $imageName) use (&$imagesForDownload) {
+                $imagesForDownload[$imageName]['is_lacking'] = true;
+            }
+        ]);
+
+        $promise = $pool->promise();
+        $promise->wait();
+
+        foreach ($imagesForDownload as $imageName => $imageData) {
+            if ($imageData['is_lacking'] === true) {
+                if (is_readable($imageData['download_to'])) {
+                    unlink($imageData['download_to']);
+                }
+                continue;
+            }
+            if (is_readable($imageData['download_to']) && filesize($imageData['download_to']) === 0) {
+                $imagesForDownload[$imageName]['is_lacking'] = true;
+                unlink($imageData['download_to']);
+            }
+        }
+
+        return $imagesForDownload;
+    }
+
+    /**
      * @param array $images
      * @param bool $debugInfo
-     * @return array|null
+     *
+     * @return null|array
      */
     public function cleanTempFiles(array $images, bool $debugInfo = false): ?array
     {
@@ -77,14 +120,5 @@ class ImageLoader
         }
 
         return $debugInfo ? $debug : null;
-    }
-
-    /**
-     * @param string $imageName
-     * @return string
-     */
-    private function generateTempFileName(string $imageName): string
-    {
-        return tempnam(sys_get_temp_dir(), str_replace('.', '_', uniqid('vms_' . microtime(true) . '_' . $imageName)));
     }
 }
